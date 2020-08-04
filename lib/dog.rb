17944f0cabf151150c638d226a2cce33bb46d421
David Gonzales <dsg2386@gmail.com>
2020-08-04 16:50:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,10 +4,10 @@ class Dog
    @breed = breed
  end
  
- def initialize 
-   @this_dogs_name = this_dogs_name
-   @breed = breed
- end
+ #def initialize 
+   #@this_dogs_name = this_dogs_name
+  # @breed = breed
+ #end
 end 
 
 fido = Dog.new 
